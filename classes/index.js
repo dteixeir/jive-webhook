e5dd55394fe5422ae64e802c837c68ea1c8782ef
Danny Teixeira <dteixeira@itadmins-MacBook-Pro-3.local>
2016-09-12 17:22:34 -0400

    initial code commit

---
+++ classes/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    //slackBot: require('./slackBot.js')
+};
---
+++ classes/slackBot.js
@@ -0,0 +1,27 @@
+
+var Bot = require('slackbots');
+var slackbot;
+var headers = '';
+var body = '';
+
+// create a bot
+// temporary fix until making it an app and using push notifications
+var bot = {
+  message: function(username, headers, body) {
+    this.headers = headers;
+    this.body = body;
+
+    
+    slackbot = new Bot({
+      token: 'xoxb-78860780950-vR0Tga9ox1rG2LIhHRnzLNOc',
+      name: 'jivebot'
+    });
+
+    slackbot.on('start', function() {
+      slackbot.postMessageToUser('danny', "headers -> " + headers);
+      slackbot.postMessageToUser('danny', "body -> " + body);
+    });
+  }
+}
+
+module.exports = bot;
