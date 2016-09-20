1257ec93db04bd236f5f9b87af7b6db0ebe794f6
Danny Teixeira <dteixeira@itadmins-MacBook-Pro-3.local>
2016-09-20 09:01:47 -0400

    extended endpoints to include adding and removing users

--- classes/slackBot.js
+++ classes/slackBot.js
@@ -3,6 +3,7 @@ var Bot = require('slackbots');
 var slackbot;
 var headers = '';
 var body = '';
+var users = [];
 
 // create a bot
 // temporary fix until making it an app and using push notifications
@@ -17,13 +18,37 @@ var bot = {
       name: 'jivebot'
     });
 
-    slackbot.on('start', function() {
-      slackbot.postMessageToUser('danny', " --------------- headers --------------- \n" + headers);
-      slackbot.postMessageToUser('danny', "--------------- body --------------- \n" + body);
-
-      slackbot.postMessageToUser('leo', " --------------- headers --------------- \n" + headers);
-      slackbot.postMessageToUser('leo', "--------------- body --------------- \n" + body);
+    slackbot.on('start', function () {
+      for (var i = 0; i < users.length; i++) {
+        slackbot.postMessageToUser(users[i], " --------------- headers --------------- \n" + headers);
+        slackbot.postMessageToUser(users[i], "--------------- body --------------- \n" + body);
+      }
     });
+  },
+
+  addUser: function (username) {
+    if (username) {
+      users.push(username);
+      return true;
+    }
+    else
+      return false;
+  },
+
+  removeUser: function (username) {
+    if (username) {
+      var i = users.indexOf(username);
+      if(i != -1) {
+        users.splice(i, 1);
+        return true;
+      }
+    }
+
+    return false;
+  },
+
+  getUsers: function () {
+    return users;
   }
 }
 
--- controllers/webhook.js
+++ controllers/webhook.js
@@ -12,6 +12,27 @@ module.exports = function (app, route) {
         res.send({ status: 200 });
     });
 
+    app.post("/webhook/add/:username", function (req, res, next) {
+        if (slackbot.addUser(req.params.username)) {
+            res.send({ status: 200 });
+        }
+        
+        else
+            res.send({ status: 400 });
+    });
+
+    app.post("/webhook/remove/:username", function (req, res, next) {
+        if (slackbot.removeUser(req.params.username)) {
+            res.send({ status: 200 });
+        }
+        else
+            res.send({ status: 400 });
+    });
+
+    app.get("/webhook/users", function (req, res, next) {
+        res.send({ data: JSON.stringify(slackbot.getUsers()) });
+    });
+
     // Return middleware ?? per use case stuff?
     return function(req, res, next) {
         next();
